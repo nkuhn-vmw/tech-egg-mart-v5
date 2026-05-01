9ddbed3ed3417ff7c68361aeda6d4d83681f68de
vcap <vcap@116507f9-fd1c-4374-6c58-c32a>
2026-05-01 19:57:30 +0000

    Add Category and Review entities and update Product imports

--- src/main/java/com/techegg/domain/Product.java
+++ src/main/java/com/techegg/domain/Product.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Column;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
