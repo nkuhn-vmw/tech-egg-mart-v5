17fc126957d0c9596fe54a27874fca49c745b138
runner1 <runner1@kuhn-labs.com>
2026-05-01 15:49:51 -0400

    reset to scaffold for v5 e2e rerun

---
+++ src/main/java/com/techegg/TechEggMartApplication.java
@@ -0,0 +1,11 @@
+package com.techegg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEggMartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechEggMartApplication.class, args);
+    }
+}
